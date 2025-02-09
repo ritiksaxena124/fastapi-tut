12726c58fb4b26472400676e7ec4af4ae4c8223a
Ritik Saxena <ritiksaxena124@gmail.com>
2025-02-09 15:51:16 +0530

    Add FastAPI application with some random endpoints for learning
    Signed-off-by: Ritik Saxena <ritiksaxena124@gmail.com>

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root() -> dict:
+    return {"message": "Hello World"}
+
+
+@app.get("/greet/{name}")
+async def greet(name: str) -> dict:
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/greet")
+async def greet_query(name: Optional[str] = None) -> dict:
+    return {"message": f"Hello {name}"}
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+
+
+@app.post("/books")
+async def add_book(book: Book) -> dict:
+    return {
+        "message": f"Book '{book.title}' whose author is '{book.author}' is added to library",
+    }
