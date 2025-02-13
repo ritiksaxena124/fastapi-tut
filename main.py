c6440f4861fc9011d3b446d6fadb79f55a5b2c7f
ritiksaxena124 <ritiksaxena124@gmail.com>
2025-02-13 22:02:15 +0530

    Add post creation and retrieval endpoints to FastAPI application

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Packages import
 from fastapi import FastAPI
 from typing import Optional
+from uuid import uuid4
 
 # Application imports
 from api.v1.books import books_router
@@ -10,6 +11,17 @@ app = FastAPI()
 
 # Include routers
 app.include_router(books_router, prefix="/books", tags=["books"])
+
+
+posts = [
+    {
+        "id": "f1f4df8a-9f69-4f90-a1f5-7569cbeabe1d",
+        "title": "Title 1",
+        "content": "Content of post 1",
+        "author": "Anonymous",
+        "rating": 5,
+    }
+]
 
 
 @app.get("/")
@@ -30,9 +42,32 @@ async def greet_query(name: Optional[str] = None) -> dict:
 @app.post("/create_post")
 async def create_post(post: Post) -> dict:
     try:
+        new_post = post.dict()
+        new_post["id"] = uuid4()
+        posts.append(new_post)
         return {
-            "message": post.dict(),
+            "data": posts,
         }
     except Exception as e:
         print(f"Error while creating post: {e}")
+        return {"error": e}
+
+
+@app.get("/posts")
+async def get_posts():
+    try:
+        return {"data": posts}
+    except Exception as e:
+        print(f"Error in getting all posts: {e}")
+
+
+@app.get("/posts/{post_id}")
+async def get_post(post_id: str):
+    try:
+        for post in posts:
+            if post["id"] == post_id:
+                return {"data": post}
+        return {"message": f"Post with id: {post_id} doesn't exists"}
+    except Exception as e:
+        print(f"Error in getting post by id: {e}")
         return {"error": e}
